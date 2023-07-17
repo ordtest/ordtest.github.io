d2e1a59fadd4dfe830afdb0f069dcc366c25641d
Gerald Bauer <gerald.bauer@gmail.com>
2023-07-17 12:38:07 +0200

    up

--- sandbox/download.rb
+++ sandbox/download.rb
@@ -8,14 +8,18 @@
 require 'ordinals'
 
 ids = [
-  ## recursive punk no.0
+  ## recursive punk no.0  - 24x24px
   '7fbfed7e38bbcaf851c2c4426b4c4ede9d09b6e6fd87a040403852681c7c19c2i0',
   '128d1e42f85e67d2a6b0c4c78e9ad47f7d66ed7699e77ddd0a4f0b8a21cdf736i0',
   '62ec589582651e49ed722241dab3bdb93fbd8ddf3fab3d891a30165126dddbcfi0',
   'b866e8f3f51c385a3ae27e65c5abe68e6086a6b74935b1b692de8587f5858148i0',
 
-  ## d.i.y. punks (spritesheet)
+  ## d.i.y. punks (spritesheet)  - 240x144px
   'cf5df319bbe23fa3d012e5ee0810700c8e82aebff41164246f0d87d7b60a9903i0',
+
+  ## ordinal punk no. 1 (flow stay)  - 192x192px (8x)
+  '8c1ab6c146389d5f4c6d4a5783284ad5e35265e75d27dd6a12a73c2d7260d86ei0',
+  '1f7b1e79290091b771b2c78b2b0275efd09c6faf3090d05162a66f491c9f247bi0',
 ]
 
 
