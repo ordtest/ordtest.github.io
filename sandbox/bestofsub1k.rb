73b8d49eb7f4566edf35295ba668577dec6d72f9
Gerald Bauer <gerald.bauer@gmail.com>
2023-07-19 17:33:44 +0200

    up

--- sandbox/bestofsub1k.rb
+++ sandbox/bestofsub1k.rb
@@ -17,6 +17,22 @@ inscribes = [
   ['№2  - image/gif, 9371 bytes  - 128×128px', false,
    'c17dd02a7f216f4b438ab1a303f518abfc4d4d01dcff8f023cf87c4403cb54cai0'],
 
+  [ '№19 - image/png, 2622 bytes - 256×288px' , true,  # pixelate
+   '9163af650dcdeeeb9a7e1f47f693b51921dce3bdf2475e69360ec83d9956f5d7i0'],
+  [ '№23 - image/png, 54889 bytes - 960×540px', false,
+   '0c107942e4c945b6b44e193c950b0fae65d854a88b450758a3fca54017c879bei0'],
+
+  [ '№46 - image/jpeg, 28377 bytes - 609×900px', false,
+   'e3e29332b269d0ae3fa28ac80427065d31b75f2c92baa729a3f8de363a0d66f6i0'],
+  [ '№48 - image/png, 36832 bytes - 1575×1050px', false,
+   '54e47c2f0e75142d1d7a0fe585e81aceb8759b07c6cfe9fa1a0bd6c317eae8b3i0'],
+
+   ['№52 - image/png, 29294 bytes - 594×553px', false,
+      'ab2f4e9dce0583264078428a91aa9037da0e75f90dc77fe3cba7cf5320ad003di0'],
+
+  ['№69 - image/png, 21670 bytes  - 415×333px', false,
+   'cd55a39d881663508a9340bafb4e860f016f16cb07bf6d34cf40bf5a62adf57bi0'],
+
 
   ['№70 - image/jpeg, 51146 bytes  - 600×502px', false, 
    'b1c5baa2593b256068635bbc475e0cc439d66c2dcf12e9de6f3aaeaf96ff818bi0'], 
@@ -28,8 +44,22 @@ inscribes = [
   ['№99 - image/png, 6727 bytes - 512×512px', true, # pixelate
    '0d495be21c9aeda05a115735dcd8872df052bf54fa382ad39bed0f5efcdb4cfei0'], 
    
+  ['№388 - image/jpeg, 389858 bytes - 2355×1864px', false,
+   '23b8c569b78b545c2d565d60f4dbf835c4adb7710d59627ed1bcf451d6b3fef6i0'],
+
+  ['№467 - image/jpeg, 144783 bytes - 1002×1332px', false,
+   'e16574cf0cec2edaaa77eff03176a3aee8e8afefa65b3732f03189d0b028f16ei0'],
+
   ['№573 - image/png, 381 bytes  - 96×96px', true,  # pixelate
-   '96d87d7e59d75ebc0e6144b09fdd96355fcdaa86fd098d64c46f19a424012bbei0'], 
+   '96d87d7e59d75ebc0e6144b09fdd96355fcdaa86fd098d64c46f19a424012bbei0'],
+  ['№586 - image/jpeg, 267930 bytes - 725×1080px', false,
+   '462dc9975167210f6ef0edf8b811a42df531e1b82774c3956a9b9d3c79e02365i0'],
+  ['№592 - image/png, 538 bytes  - 96×96px', true,  # pixelate
+   '5280166f62aa1893666f6d3d5d4e9c0a4b180c59682aae2dbdf84bf45b19030fi0'],
+
+  ['№646 - image/jpeg, 24371 bytes -  1000×1000px', true,  # pixelate
+   '1b53b2e2ea3dbe7dd9509c0e1b4e97b2f03dd3acbb7e505d09c142d5387eba83i0'
+  ], 
 ]
 
 puts "  #{inscribes.size} inscribe(s)"
